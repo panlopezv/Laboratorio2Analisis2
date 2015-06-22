37e057e4689ac4bca57d83c36f25ed8391548df6
EST1629311 <EST1629311@192.168.56.1>
2015-06-22 11:41:30 -0600

    Se instanciaron los objetos Controlador en la clase conexionespecifica

--- src/laboratorio2/ConexionEspecifica.java
+++ src/laboratorio2/ConexionEspecifica.java
@@ -4,15 +4,23 @@
  * and open the template in the editor.
  */
 package laboratorio2;
+
+import controladores.AutorJpaController;
+import controladores.LibroJpaController;
+
 /**
  *
  * @author EST1629311
  */
 public class ConexionEspecifica extends Conexion{
     private static ConexionEspecifica unicaConexion;
+    private AutorJpaController controladorAutor;
+    private LibroJpaController controladorLibro;
     
     private ConexionEspecifica(String persistenceUnitName) {
-        iniciarConexion(persistenceUnitName);        
+        iniciarConexion(persistenceUnitName);  
+        controladorAutor=new AutorJpaController(getEmf());
+        controladorLibro= new LibroJpaController(getEmf());
     }
     
     public static ConexionEspecifica getConexionEspecifica(String persistenceUnitName){
